29f0d3a6d40a6a2e676fe72a4203cd74ec9051ab
s30278 <s30278@pjwstk.edu.pl>
2024-11-17 16:18:03 +0100

    Dodanie zadań z JS

---
+++ Zad_JS_AB/JS_AB.js
@@ -0,0 +1,40 @@
+/*
+ var a;
+var b;
+function zad1(a, b){
+    var wynik = a + b;
+    return wynik;
+}
+console.log(zad1(1, 2));
+*/
+
+// Zadanie 1 - Trójka Pitagorasa
+var a;
+var b;
+var c;
+
+console.log(isPitagoras(3,4,5))
+
+function isPitagoras(a,b,c){
+    if(a*a+b*b==c*c){
+        console.log("Liczby są 'trójką pitagorasa'");
+    }
+    else{
+            console.log("Liczby nie są 'trójką pitagorasa'");
+    }
+    
+}
+
+// Zadanie 2 - Liczby a-b podzielne przez C
+
+function isDevided(){
+for(i = ; i < Array; i++){
+    console.log(i)
+    if(tab % d == 0){
+    
+    }
+}
+}
+
+//console.log(tab);
+
