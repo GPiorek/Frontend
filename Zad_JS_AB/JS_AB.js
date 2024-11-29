f53347257e53bbd78efa5e7e217d265f7ab61969
GPiorek <GrzegorzPiorekkk@outlook.com>
2024-11-29 11:34:05 +0100

    Adding JS learning file

--- Zad_JS_AB/JS_AB.js
+++ Zad_JS_AB/JS_AB.js
@@ -1,40 +1,105 @@
-/*
- var a;
-var b;
-function zad1(a, b){
-    var wynik = a + b;
-    return wynik;
-}
-console.log(zad1(1, 2));
-*/
+console.log("1.Trójka Pitagorasa")
 
-// Zadanie 1 - Trójka Pitagorasa
-var a;
-var b;
-var c;
-
-console.log(isPitagoras(3,4,5))
-
-function isPitagoras(a,b,c){
-    if(a*a+b*b==c*c){
+function isPitagoras() {
+    let a;
+    let b;
+    let c;
+    if (a * a + b * b == c * c) {
         console.log("Liczby są 'trójką pitagorasa'");
-    }
-    else{
-            console.log("Liczby nie są 'trójką pitagorasa'");
-    }
-    
-}
-
-// Zadanie 2 - Liczby a-b podzielne przez C
-
-function isDevided(){
-for(i = ; i < Array; i++){
-    console.log(i)
-    if(tab % d == 0){
-    
+    } else {
+        console.log("Liczby nie są 'trójką pitagorasa'");
     }
 }
+
+isPitagoras(3, 4, 5)
+
+
+console.log("2.liczby z przedzialu a-b podzielne przez c")
+
+function isDevided() {
+    let a = 2;
+    let b = 10;
+    let c = 5;
+    for (let i = a; i <= b; i++) {
+        if (i % c === 0) {
+            console.log(i)
+        }
+    }
 }
 
-//console.log(tab);
+isDevided()
 
+
+//Zad 3 Tabliczka mnożenia
+console.log("3.Tabliczka mnożenia")
+
+function multiplicationTable(size) {
+    for (let i = 1; i < size; i++) {
+        let row = ''
+        for (let j = 1; j < size; j++) {
+            row += i * j + '\t';
+        }
+        console.log(row);
+    }
+}
+
+multiplicationTable(5);
+
+console.log("4.Ciąg fibonacciego")
+
+function fibonacci(range) {
+    let j = 1
+    for (let i = 1; i <= range; i++) {
+        console.log(i)
+        j += i;
+        i = j - i;
+    }
+
+}
+
+fibonacci(21)
+
+console.log("5.Choinka")
+
+function christmasTree(size) {
+    for (let i = 1; i <= size; i++) {
+        let k = ' ';
+        for (let j = 1; j <= i; j++) {
+            k += '*';
+        }
+        console.log(k);
+
+    }
+}
+
+christmasTree(5)
+
+console.log("6.Choinka nocą")
+
+function christmasTreeAtNight(height) {
+        for (let i = 0; i < height; i++){
+            let tree = ''
+            for (let j = 0; j < height - i; j++) {
+                tree += '*';
+            }
+            for (let k = 0; k < i*2; k++) {
+                tree += " "
+            }
+            for (let l = 0; l < height - i; l++) {
+                 tree += '*'
+            }
+            console.log(tree);
+        }
+}
+
+christmasTreeAtNight(5)
+
+
+console.log("Funkcja licząca pole figury")
+
+function calculationArea() {
+    let a =
+    switch (size) {
+        case 1(trapez):
+    }
+}
