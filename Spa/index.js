f298cb38acb84c75b5159993c949a1550e10b04c
GPiorek <GrzegorzPiorekkk@outlook.com>
2025-01-10 23:55:26 +0100

    Whole navbar with popups done

--- Spa/index.js
+++ Spa/index.js
@@ -14,4 +14,33 @@ document.addEventListener("DOMContentLoaded", function () {
 document.getElementById("popUpOfferMobile").onclick = function () {
     document.getElementById("popUpMain").style.display = "block";
 }
+
+let popUpMainGallery = document.getElementById("popUpMainGallery");
+    let openBtnGallery = document.getElementById("popUpOpenGallery");
+    openBtnGallery.onclick = function () {
+        document.getElementById("popUpMainGallery").style.display = "block";
+    }
+
+    let closeBtnGallery = document.getElementById("popUpCloseGallery");
+    closeBtnGallery.onclick = function () {
+        document.getElementById("popUpMainGallery").style.display = "none";
+    }
+
+    let popUpMainContact = document.getElementById("popUpMainContact");
+    let openBtnContact = document.getElementById("popUpOpenContact");
+    openBtnContact.onclick = function () {
+        document.getElementById("popUpMainContact").style.display = "block";
+    }
+
+    let closeBtnContact = document.getElementById("popUpCloseContact");
+    closeBtnContact.onclick = function () {
+        document.getElementById("popUpMainContact").style.display = "none";
+    }
+
+    let homeBtn = document.getElementById("Home")
+    homeBtn.onclick = function () {
+        window.location.reload()
+    }
+
+
 });
